ecf1bc4b70990f50f7c1b93fa2ed90d77e45549e
nxn404-n <nafeurrahmanadil@gmail.com>
2025-01-15 22:01:07 +0600

    Add basic express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+// external imports
+import express from 'express';
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
