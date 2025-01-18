38b9de6001b046cac20b0422aa0c0409749e634f
nxn404-n <nafeurrahmanadil@gmail.com>
2025-01-18 14:08:14 +0600

    Add mongoose

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // external imports
 import express from 'express';
 import dotenv from "dotenv";
+import mongoose from 'mongoose';
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+  .then(console.log("Connected to MongoDB successfully!!"))
+  .catch((err) => {console.log(err)})
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
